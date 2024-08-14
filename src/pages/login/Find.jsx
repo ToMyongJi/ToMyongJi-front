08af613ed8c0fd815c9e388a038db77b9bf2d316
JunnKyuu <junnkyuu22@gmail.com>
2024-08-14 10:48:59 +0900

    feat: Login, SignUp, Find 페이지 구현

--- src/pages/login/Find.jsx
+++ src/pages/login/Find.jsx
@@ -1,11 +1,88 @@
-import React from 'react';
 import Header from '../../components/Header';
 
 const Find = () => {
   return (
-    <div className="max-w-[600px] min-h-screen ml-auto mr-auto bg-white">
+    <div className="max-w-[600px] min-h-screen mx-auto bg-white flex flex-col">
       <Header />
-      <div>Find</div>;
+      <div className="flex flex-col items-center justify-center px-4 sm:px-20 py-10 mt-3 font-GmarketLight text-[10px] sm:text-[12px]">
+        {/* 아이디 찾기 */}
+        <div className="w-full mb-8 p-4 sm:p-6 rounded-md shadow-[0_0_10px_#CED3FF]">
+          <h2 className="font-GmarketMedium text-[#002e72] text-[15px] sm:text-[18px] mb-4">아이디 찾기</h2>
+          <div className="flex flex-col p-2 space-y-6">
+            <div className="flex flex-wrap items-center">
+              <label className="w-full sm:w-[65px] text-[#002e72] mb-2 sm:mb-0">이름</label>
+              <input
+                type="text"
+                className="w-full sm:w-[calc(100%-65px)] p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#CED3FF]"
+              />
+            </div>
+            <div className="flex flex-wrap items-center">
+              <label className="w-full sm:w-[65px] text-[#002e72] mb-2 sm:mb-0">이메일 주소</label>
+              <div className="w-full sm:w-[calc(100%-65px)] flex flex-col sm:flex-row">
+                <input
+                  type="email"
+                  className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#CED3FF] mb-2 sm:mb-0 sm:mr-2"
+                />
+                <button className="w-full sm:w-auto px-4 py-2 text-white bg-[#CED3FF] rounded shadow-[0_4px_6px_rgba(156,163,255,0.5)] hover:shadow-[0_6px_8px_rgba(156,163,255,0.7)] transition-shadow duration-300 whitespace-nowrap">
+                  인증번호 발송
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-wrap items-center">
+              <label className="w-full sm:w-[65px] text-[#002e72] mb-2 sm:mb-0">인증번호</label>
+              <input
+                type="text"
+                className="w-full sm:w-[calc(100%-65px)] p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#CED3FF]"
+              />
+            </div>
+            <button className="w-full py-2 text-white bg-[#CED3FF] rounded shadow-[0_4px_6px_rgba(156,163,255,0.5)] hover:shadow-[0_6px_8px_rgba(156,163,255,0.7)] transition-shadow duration-300">
+              인증확인
+            </button>
+          </div>
+        </div>
+        {/* 비밀번호 찾기 삭제 예정
+        <div className="w-full p-4 sm:p-6 rounded-md shadow-[0_0_10px_#CED3FF] mt-5">
+          <h2 className="font-GmarketMedium text-[#002e72] text-[15px] sm:text-[18px] mb-4">비밀번호 찾기</h2>
+          <div className="flex flex-col p-2 space-y-6">
+            <div className="flex flex-wrap items-center">
+              <label className="w-full sm:w-[65px] text-[#002e72] mb-2 sm:mb-0">아이디</label>
+              <input
+                type="text"
+                className="w-full sm:w-[calc(100%-65px)] p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#CED3FF]"
+              />
+            </div>
+            <div className="flex flex-wrap items-center">
+              <label className="w-full sm:w-[65px] text-[#002e72] mb-2 sm:mb-0">이름</label>
+              <input
+                type="text"
+                className="w-full sm:w-[calc(100%-65px)] p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#CED3FF]"
+              />
+            </div>
+            <div className="flex flex-wrap items-center">
+              <label className="w-full sm:w-[65px] text-[#002e72] mb-2 sm:mb-0">이메일 주소</label>
+              <div className="w-full sm:w-[calc(100%-65px)] flex flex-col sm:flex-row">
+                <input
+                  type="email"
+                  className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#CED3FF] mb-2 sm:mb-0 sm:mr-2"
+                />
+                <button className="w-full sm:w-auto px-4 py-2 text-white bg-[#CED3FF] rounded shadow-[0_4px_6px_rgba(156,163,255,0.5)] hover:shadow-[0_6px_8px_rgba(156,163,255,0.7)] transition-shadow duration-300 whitespace-nowrap">
+                  인증번호 발송
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-wrap items-center">
+              <label className="w-full sm:w-[65px] text-[#002e72] mb-2 sm:mb-0">인증번호</label>
+              <input
+                type="text"
+                className="w-full sm:w-[calc(100%-65px)] p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#CED3FF]"
+              />
+            </div>
+            <button className="w-full py-2 text-white bg-[#CED3FF] rounded shadow-[0_4px_6px_rgba(156,163,255,0.5)] hover:shadow-[0_6px_8px_rgba(156,163,255,0.7)] transition-shadow duration-300">
+              인증확인
+            </button>
+          </div>
+        </div> */}
+      </div>
     </div>
   );
 };
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,58 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useCallback } from 'react';
+
 import Header from '../../components/Header';
+import logo from '../../assets/images/logo.png';
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const handleOnClick = useCallback(
+    (path) => {
+      return () => navigate(path);
+    },
+    [navigate]
+  );
+
   return (
-    <div className="max-w-[600px] min-h-screen ml-auto mr-auto bg-white">
+    <div className="max-w-[600px] min-h-screen mx-auto bg-white flex flex-col">
       <Header />
-      <span>login</span>
+      <div className="flex items-center justify-center px-20 py-10 mt-5 font-GmarketLight text-[7.5px] sm:text-[13px]">
+        <div className="flex flex-col items-center px-10 py-4 rounded-md shadow-[0_0_10px_#CED3FF]">
+          <img src={logo} alt="로고" className="w-[40%] sm:w-[50%] mb-6" />
+          <input
+            type="text"
+            placeholder="아이디"
+            className="text-[#002e72] p-2 mb-4 border rounded-lg w-[90%] focus:outline-none focus:ring-2 focus:ring-[#CED3FF]"
+          />
+          <input
+            type="password"
+            placeholder="비밀번호"
+            className="text-[#002e72] p-2 mb-4 border rounded-lg w-[90%] focus:outline-none focus:ring-2 focus:ring-[#CED3FF]"
+          />
+          <button className=" w-[90%] py-2 mb-4 text-white bg-[#CED3FF] border-none rounded shadow-[0_4px_6px_rgba(156,163,255,0.5)] hover:shadow-[0_6px_8px_rgba(156,163,255,0.7)] transition-shadow duration-300">
+            로그인
+          </button>
+          <div className="flex justify-between w-[90%] mb-4">
+            <label className="flex items-center text-[#002e72]">
+              <input type="checkbox" className="mr-2 accent-[#CED3FF]" />
+              로그인 유지
+            </label>
+            <button
+              className="text-[#002e72] hover:text-[#CED3FF] transition duration-300"
+              onClick={handleOnClick('/find')}
+            >
+              아이디/비밀번호 찾기
+            </button>
+          </div>
+          <button
+            className="text-[#002e72] hover:text-[#CED3FF] transition duration-300"
+            onClick={handleOnClick('/sign-up')}
+          >
+            회원가입
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
