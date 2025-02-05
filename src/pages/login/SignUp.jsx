7ecfe5767e55e37d54c54c2e56cdb92694580864
JunnKyuu <junnkyuu22@gmail.com>
2025-02-05 13:41:58 +0900

    Fix: 회원가입 필드 수정

--- src/pages/login/SignUp.jsx
+++ src/pages/login/SignUp.jsx
@@ -242,7 +242,7 @@ const SignUp = () => {
 
             <hr className="border-t border-[#eeeffe]" />
 
-            {/* 이메일/인증코드  */}
+            {/* 이름/이메일/인증코드  */}
             <div className="py-3 space-y-7">
               {/* 이메일 */}
               <div className="flex flex-wrap items-center">
@@ -292,7 +292,7 @@ const SignUp = () => {
 
             <hr className="border-t border-[#eeeffe]" />
 
-            {/* 이름/학번/대학/자격 파트 */}
+            {/* 학번/대학/학과/자격 파트 */}
             <div className="py-4 space-y-7">
               {/* 이름 */}
               <div className="flex flex-wrap items-center">
@@ -345,19 +345,19 @@ const SignUp = () => {
                 </select>
               </div>
             </div>
-            {/* 소속부원 관리 */}
-            <div className="flex flex-wrap items-center mb-4">
-              <label className="w-full sm:w-[120px] text-[#002e72] mb-2 sm:mb-0">소속부원 관리</label>
-              <div className="w-full sm:w-[calc(100%-120px)] flex flex-col sm:flex-row gap-2">
+            {/* 소속 이름 */}
+            <div className="flex flex-wrap items-center">
+              <label className="w-full sm:w-[65px] text-[#002e72] mb-2 sm:mb-0">소속 이름</label>
+              <div className="w-full sm:w-[calc(100%-65px)] flex flex-col sm:flex-row">
                 <select
                   value={selectedClub}
                   onChange={(e) => setSelectedClub(e.target.value)}
-                  className="w-full sm:flex-1 p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#CED3FF] mb-2 sm:mb-0"
+                  className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#CED3FF] mb-4 sm:mb-0 sm:mr-2"
                 >
-                  <option value="">선택하세요</option>
+                  <option value="">소속을 선택해주세요.</option>
                   {clubs.map((club) => (
-                    <option key={club.id} value={club.id}>
-                      {club.name}
+                    <option key={club.studentClubId} value={club.studentClubId}>
+                      {club.studentClubName}
                     </option>
                   ))}
                 </select>
@@ -369,6 +369,7 @@ const SignUp = () => {
                   소속 인증하기
                 </button>
               </div>
+              {isClubVerified && <p className="text-[#4CAF50] text-[9px] sm:text-xs mt-1">소속 인증 완료</p>}
             </div>
 
             <hr className="sm:hidden border-t border-[#eeeffe]" />
