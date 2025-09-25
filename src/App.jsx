7647000ce120470ccf354bbf6a137bca8a790a9d
rhdmschdl <lovelyskygir@naver.com>
2025-09-25 21:54:53 +0900

    chore: 0927 점검기간 설정

--- src/App.jsx
+++ src/App.jsx
@@ -76,8 +76,8 @@ const App = () => {
   }, [authData, user, setUser]);
 
   // 점검 기간 설정
-  const maintenanceStart = new Date("2025-04-12T15:00:00");
-  const maintenanceEnd = new Date("2025-04-13T00:00:00");
+  const maintenanceStart = new Date("2025-09-27T00:00:00");
+  const maintenanceEnd = new Date("2025-09-27T04:00:00");
   const now = new Date();
   const isMaintenance = now >= maintenanceStart && now <= maintenanceEnd;
 
