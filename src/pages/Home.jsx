7f86e98205a7dfdecebf3280ad08881540349dca
rhdmschdl <lovelyskygir@naver.com>
2025-10-03 00:00:03 +0900

    feat: 홈화면 문구 변경 및 이미지 최신화

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
-import { useEffect } from 'react';
-import Header from '../components/Header';
-import IntroduceCard from '../components/IntroduceCard';
-import Footer from '../components/Footer';
-import IntroduceImageCard from '../components/IntroduceImageCard';
+import { useEffect } from "react";
+import Header from "../components/Header";
+import IntroduceCard from "../components/IntroduceCard";
+import Footer from "../components/Footer";
+import IntroduceImageCard from "../components/IntroduceImageCard";
 
-import logo from '../assets/images/logo.png';
-import receiptList from '../assets/images/receipts-list.png';
-import createReceipt from '../assets/images/create-receipt.png';
-import myPage from '../assets/images/my-page.png';
+import logo from "../assets/images/logo.png";
+import receiptList from "../assets/images/receipts-list.png";
+import createReceipt from "../assets/images/create-receipt.png";
+import myPage from "../assets/images/my-page.png";
 
 const Home = () => {
   useEffect(() => {
@@ -30,36 +30,52 @@ const Home = () => {
       <div className="flex flex-col items-center justify-center p-10">
         {/* 투명지 소개 */}
         <div className="w-full sm:w-[90%] flex items-center justify-between mb-[100px]">
-          <img className="w-[45%] sm:w-[40%]" src={logo} alt="투명지 로고 이미지" />
+          <img
+            className="w-[45%] sm:w-[40%]"
+            src={logo}
+            alt="투명지 로고 이미지"
+          />
           <div className="w-[50%] text-[#061E5B] text-[12px] sm:text-[14px] flex flex-col justify-between py-2 font-GmarketLight h-[115px] sm:h-[170px]">
-            <p>투명한 명지의 줄임말로, 학생회비 사용 내역을 투명하고 간편하게 조회할 수 있는 서비스입니다.</p>
+            <p>
+              투명한 명지의 줄임말로, 학생회비 사용 내역을 투명하고 간편하게
+              조회할 수 있는 서비스입니다.
+            </p>
             <br />
-            <p>영어로는 To MyongJi 로, 명지대 학생들에게 라는 의미 또한 가지고 있습니다 :)</p>
+            <p>
+              영어로는 To MyongJi 로, 명지대 학생들에게 라는 의미 또한 가지고
+              있습니다 :)
+            </p>
           </div>
         </div>
         {/* 기능 소개 */}
         {/* 조회 버튼 */}
         <div className="w-full sm:w-[90%] flex items-center justify-between mb-[100px]">
-          <div className="w-[45%]">
-            <IntroduceCard
-              contentText={'화면 메뉴의 조회버튼을 클릭하여 원하는 대학과 소속을 선택하면 조회가 가능합니다.'}
-              buttonText={'메뉴바의 조회를 통해 학생회비 사용 내역 조회 하러 가기'}
-            />
+          <div className="w-[45%] text-[#061E5B] font-GmarketLight text-[12px] sm:text-[14px] p-1">
+            메뉴의 조회를 클릭하여 원하는 대학과 소속을 선택하면 해당 학생회의
+            학생회비 사용 내역 조회가 가능합니다.
           </div>
           <div className="w-[45%] flex justify-end">
-            <IntroduceImageCard image={receiptList} />
+            <IntroduceImageCard
+              image={receiptList}
+              text={"*해당 이미지는 실제 데이터가 아닌 예시 화면입니다."}
+            />
           </div>
         </div>
         {/* 작성 버튼 */}
         <div className="w-full sm:w-[90%] flex items-center justify-between mb-[100px]">
           <div className="w-[45%] flex justify-start">
-            <IntroduceImageCard image={createReceipt} />
+            <IntroduceImageCard
+              image={createReceipt}
+              text={"*해당 이미지는 실제 데이터가 아닌 예시 화면입니다."}
+            />
           </div>
           <div className="w-[45%]">
             <IntroduceCard
-              contentText={'학생회 소속일 경우 수기 또는 사진을 찍어 영수증 작성이 가능합니다.'}
-              buttonText={'학과 학생회비 사용 내역 작성 하러 가기'}
-              path={'/create-receipt'}
+              contentText={
+                "학생회 소속일 경우 수기 또는 다양한 방법을 통해 영수증 작성이 가능합니다."
+              }
+              buttonText={"학과 학생회비 사용 내역 작성 하러 가기"}
+              path={"/create-receipt"}
             />
           </div>
         </div>
@@ -68,14 +84,17 @@ const Home = () => {
           <div className="w-[45%]">
             <IntroduceCard
               contentText={
-                '학생회 소속일 경우 내 정보 관리를 할 수 있고, 학생회 회장일 경우 소속원 관리까지 할 수 있습니다.'
+                "학생회 소속일 경우 내 정보 관리를 할 수 있고, 학생회 회장일 경우 소속원 관리까지 할 수 있습니다."
               }
-              buttonText={'마이 페이지 조회 하러 가기'}
-              path={'/my-page'}
+              buttonText={"마이 페이지 조회 하러 가기"}
+              path={"/my-page"}
             />
           </div>
           <div className="w-[45%] flex justify-end">
-            <IntroduceImageCard image={myPage} />
+            <IntroduceImageCard
+              image={myPage}
+              text={"*해당 이미지는 실제 데이터가 아닌 예시 화면입니다."}
+            />
           </div>
         </div>
       </div>
